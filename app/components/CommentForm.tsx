34667081437f51a0ca5180a36e961cb6362732ec
Velraaj <velraaj30@gmail.com>
2026-02-13 17:56:50 +0530

    Some changes

--- app/components/CommentForm.tsx
+++ app/components/CommentForm.tsx
@@ -1,15 +1,24 @@
-import { TextArea } from "@radix-ui/themes";
+import { Button, TextArea } from "@radix-ui/themes";
 import { GetProfileDetails } from "../(routes)/settings/actions";
 import Avatar from "./Avatar";
+import { SendIcon, UploadCloudIcon } from "lucide-react";
+import { AddComment } from "../(routes)/posts/[id]/action";
 
 export default async function CommentForm() {
   const profile = await GetProfileDetails();
+  
   return (
-    <form action="">
+    <form action={}>
       <div className="flex gap-6">
         <Avatar profileUrl={profile.Avatar || ""} />
         <div className="w-5/6">
-          <TextArea placeholder="Comment your thoughts here..."/>
+          <TextArea placeholder="Comment your thoughts here..." name="commentContent"/>
+          <div className="mt-2">
+            <Button type="submit">
+              <SendIcon size={16}/>
+              Add Comment
+            </Button>
+          </div>
         </div>
       </div>
     </form>
